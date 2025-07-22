99199efd6073c5554c47393b65989317e21c5f6c
Aytrucks WDWS <105306690+Aytrucks@users.noreply.github.com>
2025-07-22 00:18:53 -0700

    work on battle ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div id="test_text">
-        Test
+        TEST
       </div>
       <button id="test_btn">
         TEST BUTTON
       </button>
-      <div className="poke_elec">
+      <div className="poke">
         <img src="/stuffed_pika.jpg"/>
+      </div>
+      <div class="test_input">
+        <input class="box"/>
       </div>
     </>
   )
