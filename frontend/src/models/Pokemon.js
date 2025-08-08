1e339f50b9c838a506ab1d318090907783e73073
Aytrucks <105306690+Aytrucks@users.noreply.github.com>
2025-08-08 01:07:15 -0700

    add handling for fairy type

--- frontend/src/models/Pokemon.js
+++ frontend/src/models/Pokemon.js
@@ -1,11 +1,14 @@
 class Pokemon{
     constructor(apiData){
-        this.name = apiData.name,
-        this.type1 = apiData.types[0].type.name,
-        this.type2 = apiData.types[1]?.type.name ||"none",
-        this.ability1 = apiData.abilities[0].ability.name,
-        this.ability2 = apiData.abilities[1]?.ability.name || "none",
-        this.img = apiData.sprites["other"]['official-artwork']['front_default']
+        this.name = apiData.name;
+        this.type1 = apiData.types[0].type.name === "fairy" ? "normal" : apiData.types[0].type.name;
+
+        const type2Value = apiData.types[1]?.type.name || "none";
+        this.type2 = type2Value === 'fairy' ? 'none' : type2Value;
+
+        this.ability1 = apiData.abilities[0].ability.name;
+        this.ability2 = apiData.abilities[1]?.ability.name || "none";
+        this.img = apiData.sprites["other"]['official-artwork']['front_default'];
     }
 
     getBattleData(){
